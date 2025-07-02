03829de8306450c0108bc3ef1406c7eb3b8e4aa6
davidottesen <davidottesen@gmail.com>
2025-07-02 00:52:36 -0500

    minor progress

--- rocket_simulation.py
+++ rocket_simulation.py
@@ -3,6 +3,7 @@ import astropy.units as u  # type: ignore
 from astropy.units.quantity import Quantity
 from typing import List, Tuple
 import math
+import numpy as np
 
 # Functions
 def process_input(
@@ -41,27 +42,32 @@ def process_input(
 
 def rocket_dynamics(
     time: float,
-    state: List[float],
-) -> float:
-    
-    # Unpack state: state = [vel, mass]
-    vel = state[0]
-    mass = state[1]
+    state: np.ndarray,
+    **kwargs,
+) -> np.ndarray:
 
-    spec_imp = 0.0
-    grav_acc_const = 9.81
-    exhaust_velocity = spec_imp * grav_acc_const
-    dmass__dtime = 0.0
-    
-    drag_acc = 0.0
+    pos  = state[0]
+    vel  = state[1]
+    mass = state[2]
 
-    flight_path_angle = 90.0 * math.pi / 180.0
-    grav_acc = -grav_acc_const * math.sin(flight_path_angle)
+    spec_imp           = kwargs.get('spec_imp', 0.0)
+    grav_acc_const     = kwargs.get('grav_acc_const', 9.81)
+    grav_acc_sea_level = kwargs.get('grav_acc_sea_level', 9.81)
+    thrust             = kwargs.get('thrust', 0.0)
 
-    dvel__dtime = exhaust_velocity * dmass__dtime + drag_acc + grav_acc
+    exhaust_velocity = spec_imp * grav_acc_sea_level
 
-    dstate__dtime[0] = dvel__dtime
-    dstate__dtime[1] = dmass__dtime
+    grav_acc   = -grav_acc_const
+    thrust_acc = thrust / mass
+
+    dpos__dtime  = vel
+    dvel__dtime  = grav_acc + thrust_acc # + drag_acc
+    dmass__dtime = -thrust / exhaust_velocity
+
+    dstate__dtime    = np.zeros(3, dtype=float)
+    dstate__dtime[0] = dpos__dtime
+    dstate__dtime[1] = dvel__dtime
+    dstate__dtime[2] = dmass__dtime
 
     return dstate__dtime
 
@@ -71,6 +77,7 @@ def simulate_rocket_trajectory(
     pos_vec_o: Quantity,
     mass_o: Quantity,
 ) -> None:
+    # rocket_dynamics(time, state)
     pass
 
 
