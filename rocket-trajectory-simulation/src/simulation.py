9191d77fd17f73fc1d60eabdb2e179819a3a50d7
davidottesen <davidottesen@gmail.com>
2025-07-05 00:39:00 -0500

    refactored code into more functions

--- rocket-trajectory-simulation/src/simulation.py
+++ rocket-trajectory-simulation/src/simulation.py
@@ -3,7 +3,6 @@ import astropy.units as u
 import numpy as np
 from typing import List, Tuple
 from scipy.integrate import solve_ivp
-from scipy.integrate._ivp.ivp import OdeResult
 import matplotlib.pyplot as plt
 
 from rocket_dynamics import rocket_dynamics_1d, rocket_dynamics_2d
@@ -87,7 +86,7 @@ def process_input(
     )
 
 
-def simulate_rocket_trajectory(
+def simulate_rocket_trajectory_1d(
     delta_time         : Quantity,
     time_steps         : int,
     pos_vec_o          : Quantity,
@@ -98,7 +97,7 @@ def simulate_rocket_trajectory(
     grav_acc_const     : Quantity,
     grav_acc_sea_level : Quantity,
 ) -> None:
-    
+    """Simulate and plot 1D rocket trajectory."""
     time_span        = (0, delta_time.value)
     time_eval        = np.linspace(0, delta_time.value, time_steps)
     state_o          = np.array([pos_vec_o[0].value, vel_vec_o[0].value, mass_o.value], dtype=float)
@@ -136,7 +135,6 @@ def simulate_rocket_trajectory(
     print(f"      Velocity         : {vel_vec_f[0].to_value('m/s'):>12.6f} m/s")
     print(f"      Mass             : {mass_f.to_value('kg'):>12.6f} kg")
     print(f"  System Parameters    :")
-    breakpoint()
     print(f"    Thrust             : {thrust_mag.to_value('N'):>12.6f} N") # type: ignore
     print(f"    Grav Acc Const     : {grav_acc_const.to_value('m/s^2'):>12.6f} m/s^2")
     print(f"    Specific Impulse   : {spec_imp.to_value('s'):>12.6f} s")
@@ -145,8 +143,7 @@ def simulate_rocket_trajectory(
 
     print("Plotting the results ...")
     fig, ax = plt.subplots(3, 1, figsize=(10, 6))
-
-    fig.suptitle('Rocket Trajectory Simulation')
+    fig.suptitle('Rocket Trajectory Simulation (1D)')
     fig.align_ylabels(ax)
     ax[0].set_ylabel('Position [m]')
     ax[0].grid()
@@ -157,16 +154,29 @@ def simulate_rocket_trajectory(
     ax[2].set_xlabel('Time [s]')
     ax[2].set_ylabel('Mass [kg]')
     ax[2].grid()
-
     ax[0].plot(soln.t, soln.y[0], label='Pos', color='orange')
     ax[1].plot(soln.t, soln.y[1], label='Vel', color='orange')
     ax[2].plot(soln.t, soln.y[2], label='Mass', color='black')
+    plt.show()
 
-    # 2D Rocket Simulation
+
+def simulate_rocket_trajectory_2d(
+    delta_time         : Quantity,
+    time_steps         : int,
+    pos_vec_o          : Quantity,
+    vel_vec_o          : Quantity,
+    mass_o             : Quantity,
+    thrust_vec         : Quantity,
+    spec_imp           : Quantity,
+    grav_acc_const     : Quantity,
+    grav_acc_sea_level : Quantity,
+) -> None:
+    """Simulate and plot 2D rocket trajectory."""
     time_span        = (0, delta_time.value)
     time_eval        = np.linspace(0, delta_time.value, time_steps)
     state_o          = np.hstack([pos_vec_o.value, vel_vec_o.value, mass_o.value])
     exhaust_velocity = spec_imp * grav_acc_sea_level
+    thrust_mag       = np.linalg.norm(thrust_vec)
     params           = (thrust_vec.value, exhaust_velocity.value, grav_acc_const.value)
 
     soln = solve_ivp(
@@ -182,8 +192,7 @@ def simulate_rocket_trajectory(
     vel_vec_f = soln.y[2:4,-1] * vel_vec_o.unit
     mass_f    = soln.y[4,-1] * mass_o.unit
 
-    print()
-    print("Simulation Results: 2D Rocket Trajectory")
+    print("\nSimulation Results: 2D Rocket Trajectory")
     print(f"  Success              : {soln.success}")
     print(f"  Message              : {soln.message}")
     print(f"  Time                 :")
@@ -200,7 +209,7 @@ def simulate_rocket_trajectory(
     print(f"      Velocity         : {vel_vec_f[0].to_value('m/s'):>12.6f} m/s")
     print(f"      Mass             : {mass_f.to_value('kg'):>12.6f} kg")
     print(f"  System Parameters    :")
-    print(f"    Thrust             : {thrust_mag.to_value('N'):>12.6f} N")
+    print(f"    Thrust             : {thrust_mag:>12.6f} N")
     print(f"    Grav Acc Const     : {grav_acc_const.to_value('m/s^2'):>12.6f} m/s^2")
     print(f"    Specific Impulse   : {spec_imp.to_value('s'):>12.6f} s")
     print(f"    Grav Acc Sea Level : {grav_acc_sea_level.to_value('m/s^2'):>12.6f} m/s^2")
@@ -208,7 +217,7 @@ def simulate_rocket_trajectory(
 
     print("Plotting the results ...")
     fig, ax = plt.subplots(3, 1, figsize=(10, 6))
-    fig.suptitle('Rocket Trajectory Simulation')
+    fig.suptitle('Rocket Trajectory Simulation (2D)')
     fig.align_ylabels(ax)
     ax[0].set_ylabel('Position [m]')
     ax[0].grid()
@@ -240,10 +249,7 @@ def simulate_rocket_trajectory(
     ax[1].plot(soln.y[2,0], soln.y[3,0], color='red', marker='>', markersize=5)
     ax[1].plot(soln.y[2], soln.y[3], color='red')
     ax[1].plot(soln.y[2,-1], soln.y[3,-1], color='red', marker='s', markersize=5)
-
     plt.show()
-
-
 
 # Main
 if __name__ == "__main__":
@@ -296,7 +302,19 @@ if __name__ == "__main__":
     )
 
     # Simulate
-    simulate_rocket_trajectory(
+    simulate_rocket_trajectory_1d(
+        delta_time         = delta_time,
+        time_steps         = time_steps,
+        pos_vec_o          = pos_vec_o,
+        vel_vec_o          = vel_vec_o,
+        mass_o             = mass_o,
+        thrust_vec         = thrust_vec,
+        spec_imp           = spec_imp,
+        grav_acc_const     = grav_acc_const,
+        grav_acc_sea_level = grav_acc_sea_level,
+    )
+
+    simulate_rocket_trajectory_2d(
         delta_time         = delta_time,
         time_steps         = time_steps,
         pos_vec_o          = pos_vec_o,
