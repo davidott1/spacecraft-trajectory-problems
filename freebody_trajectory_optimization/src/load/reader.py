e99e0ae0cf1b70e1b0c34c43802ff9d7beefe7d9
davidottesen <davidottesen@gmail.com>
2025-08-12 00:02:14 -0500

    add a func

--- freebody_trajectory_optimization/src/load/reader.py
+++ freebody_trajectory_optimization/src/load/reader.py
@@ -18,16 +18,30 @@ def configure_and_read():
     output_folderpath_input = parsed_cli_input['output_folderpath_input']
 
     # Read input parameters
-    use_json_reader = False
-    if input_filepath_input.endswith('.json'):
-        input_filepath = input_filepath_input
-        use_json_reader = True
-    else:
-        input_filepath = f"{input_filepath_input}.json"
-        use_json_reader = True
-    if use_json_reader:
-        input_parameters = read_json(input_filepath)
-        print(f"  Input Filepath    : {input_filepath}")
+    def read_input(
+            input_filepath_input,
+        ):
+        use_json_reader = False
+        if input_filepath_input.endswith('.json'):
+            input_filepath = input_filepath_input
+            use_json_reader = True
+        else:
+            input_filepath = f"{input_filepath_input}.json"
+            use_json_reader = True
+        if use_json_reader:
+            input_parameters = read_json(input_filepath)
+            print(f"  Input Filepath    : {input_filepath}")
+        return (
+            input_filepath, 
+            input_parameters,
+        )
+    (
+        input_filepath, 
+        input_parameters,
+    ) = \
+        read_input(
+            input_filepath_input,
+        )
 
     # Output folderpath
     output_folderpath = output_folderpath_input # do nothing for now
