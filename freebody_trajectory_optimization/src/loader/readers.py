440b090daab3871a5d8c32d200093bf1eefe5be7
davidottesen <davidottesen@gmail.com>
2025-08-11 22:05:53 -0500

    read updates

--- freebody_trajectory_optimization/src/loader/readers.py
+++ freebody_trajectory_optimization/src/loader/readers.py
@@ -9,12 +9,12 @@ def configure_and_read():
     - folderpath of the output
     """ 
 
-    # Configure input and output folders
-    print("\nConfigure Input and Output Folders")
-    parser = argparse.ArgumentParser(description="Configure input and output files and folders")
+    # Parse file and folderpath
+    print("Parse Filepaths and Folderpaths")
+    parser = argparse.ArgumentParser()
     parser.add_argument(
-        "input_filepath"                          ,
-        help            = "Path to the input file",
+        "input_filepath"               ,
+        help = "Path to the input file",
     )
     parser.add_argument(
         "output_folderpath"                                    ,
@@ -27,17 +27,17 @@ def configure_and_read():
     output_folderpath_input = args.output_folderpath
 
     # Read input parameters
+    print("\nRead Input Parameters")
     use_json_reader = False
-
     if input_filepath_input.endswith('.json'):
         input_filepath = input_filepath_input
         use_json_reader = True
     else:
         input_filepath = f"{input_filepath_input}.json"
         use_json_reader = True
-    
     if use_json_reader:
         input_parameters = read_json(input_filepath)
+        print(f"  Input  : {input_filepath}")
 
     # Output folderpath
     output_folderpath = output_folderpath_input # do nothing for now
@@ -54,6 +54,5 @@ def read_json(filename):
     
     with open(filename, "r") as file:
        parameters_input = json.load(file)
-    print(f"  Input  : {filename}")
 
     return parameters_input
