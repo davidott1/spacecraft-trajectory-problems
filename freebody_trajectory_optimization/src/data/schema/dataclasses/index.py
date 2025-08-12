084da671937c217146fb74cffcd2ca1e140a0b34
davidottesen <davidottesen@gmail.com>
2025-08-12 01:11:24 -0500

    remove a file

--- freebody_trajectory_optimization/src/data/schema/dataclasses/index.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-@dataclass
-class RocketState:
-    time     : float        # time at this state
-    position : List[float]  # position in 3D space
-    velocity : List[float]  # velocity in 3D space
-    mass     : float        # mass of the rocket
