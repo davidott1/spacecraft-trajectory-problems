fixed sign bug

one_body_trajectory_optimization/src/model/dynamics.py
@@ -29,8 +29,6 @@ def control_thrust_acceleration(
             thrust_acc_mag   = thrust_acc_min + (thrust_acc_max - thrust_acc_min) * heaviside_approx
         else: # no use_thrust_smoothing and no use_thrust_acc_smoothing
             thrust_acc_mag = np.where(switching_func > 0.0, thrust_acc_max, thrust_acc_min)
-        thrust_acc_x_dir = -covel_x * covel_mag_inv
-        thrust_acc_y_dir = -covel_y * covel_mag_inv
     else: # assume 'energy'
         thrust_acc_mag = covel_mag
         if use_thrust_limits or use_thrust_acc_limits:
@@ -38,8 +36,8 @@ def control_thrust_acceleration(
                 thrust_acc_mag = bounded_smooth_func(thrust_acc_mag, thrust_acc_min, thrust_acc_max, k_steepness)
             else: # no use_thrust_smoothing and no use_thrust_acc_smoothing
                 thrust_acc_mag = bounded_nonsmooth_func(thrust_acc_mag, thrust_acc_min, thrust_acc_max)
-        thrust_acc_x_dir = covel_x * covel_mag_inv
-        thrust_acc_y_dir = covel_y * covel_mag_inv
+    thrust_acc_x_dir = -covel_x * covel_mag_inv
+    thrust_acc_y_dir = -covel_y * covel_mag_inv
     thrust_acc_x = thrust_acc_mag * thrust_acc_x_dir
     thrust_acc_y = thrust_acc_mag * thrust_acc_y_dir
 

one_body_trajectory_optimization/src/optimize/two_point_boundary_value_problem.py
@@ -451,66 +451,67 @@ def tpbvp_objective_and_jacobian(
     error_covel_vec_f = covel_vec_f_pls - covel_vec_f_mns
 
     # Hamiltonian error
-    error_ham_o = ham_o_pls - ham_o_mns # trivial
+    error_ham_o = ham_o_pls - ham_o_mns
     error_ham_f = ham_f_pls - ham_f_mns
 
     # Full error vector
     error_full = {
         'time' : {
-            'o': error_time_o, # trivial
-            'f': error_time_f  # trivial
+            'o': error_time_o,
+            'f': error_time_f 
         },
         'pos_vec' : {
-            'o': error_pos_vec_o, # trivial
+            'o': error_pos_vec_o,
             'f': error_pos_vec_f
         },
         'vel_vec' : {
-            'o': error_vel_vec_o, # trivial
+            'o': error_vel_vec_o,
             'f': error_vel_vec_f
         },
         'copos_vec' : {
-            'o': error_copos_vec_o, # trivial
-            'f': error_copos_vec_f  # trivial
+            'o': error_copos_vec_o,
+            'f': error_copos_vec_f
         },
         'covel_vec' : {
-            'o': error_covel_vec_o, # trivial
-            'f': error_covel_vec_f  # trivial
+            'o': error_covel_vec_o,
+            'f': error_covel_vec_f
         },
         'ham' : {
-            'o': error_ham_o, # trivial
-            'f': error_ham_f  
+            'o': error_ham_o,
+            'f': error_ham_f
         }
     }
 
-    # Error vector (currently not consolidated)
+    # Error vector
     error_components = []
     for bnd in ordered_boundaries:
         for var in ordered_variables:
-            # print(f"error {var}_{bnd}: {error_full[var][bnd]}")
             error_components.append(error_full[var][bnd])
     error = np.hstack(error_components)
 
-    # start here: error is correct. need to get rid of trivial zero errors
+    # Error jacobian
     if include_jacobian:
-        # 4x4 : -d(pos_vec_f_mns, vel_vec_f_mns) / d(copos_vec_o_pls, covel_vec_o_pls)
-        # 5x5: -d(pos_vec_f_mns, vel_vec_f_mns, ham_f_mns) / d(time_f_mns, copos_vec_o_pls, covel_vec_o_pls)
+        #   error = [ delta [time_o, pos_vec_o, vel_vec_o, copos_vec_o, covel_vel_o, ham_o] ]
+        #           [ delta [time_f, pos_vec_f, vel_vec_f, copos_vec_f, covel_vel_f, ham_f] ]
+        #   jacobian = d(state_costate_f) / d(state_costate_o)
+        #   error_jacobian = d(error) / d(decision_state)
+        #                  = [ d(delta_time_o)      / d(time_o_pls)    d(delta_time_o)      / d(pos_vec_o_pls)    ...
+        #                      d(delta_pos_vec_o)   / d(time_o_pls)    d(delta_pos_vec_o)   / d(pos_vec_o_pls)    ...
+        #                      d(delta_vel_vec_o)   / d(time_o_pls)    d(delta_vel_vec_o)   / d(pos_vec_o_pls)    ...
+        #                      d(delta_copos_vec_o) / d(time_o_pls)    d(delta_copos_vec_o) / d(pos_vec_o_pls)    ...
+        #                      d(delta_covel_vel_o) / d(time_o_pls)    d(delta_covel_vel_o) / d(pos_vec_o_pls)    ...
+        #                      d(delta_ham_o)       / d(time_o_pls)    d(delta_ham_o)       / d(pos_vec_o_pls)    ...
+        #                      d(delta_time_f)      / d(time_f_pls)    d(delta_time_f)      / d(pos_vec_f_pls)    ...
+        #                      d(delta_pos_vec_f)   / d(time_f_pls)    d(delta_pos_vec_f)   / d(pos_vec_f_pls)    ...
+        #                      d(delta_vel_vec_f)   / d(time_f_pls)    d(delta_vel_vec_f)   / d(pos_vec_f_pls)    ...
+        #                      d(delta_copos_vec_f) / d(time_f_pls)    d(delta_copos_vec_f) / d(pos_vec_f_pls)    ...
+        #                      d(delta_covel_vel_f) / d(time_f_pls)    d(delta_covel_vel_f) / d(pos_vec_f_pls)    ...
+        #                      d(delta_ham_f)       / d(time_f_pls)    d(delta_ham_f)       / d(pos_vec_f_pls)    ...
+        # breakpoint()
+        # stm_of
         error_jacobian = np.zeros((len(error), len(error)))
 
-    # # Consolidate errors
-    # case_choice = 1 # 1 : fixed fin-time; fixed init-pos; fixed init-vel; fixed fin-pos; fixed fin-vel
-    #                 # 2 :  free fin-time; fixed init-pos; fixed init-vel; fixed fin-pos; fixed fin-vel
-    # if case_choice == 1:
-    #     error = np.hstack([error_pos_vec_f, error_vel_vec_f]) # 4 constraints
-    #     if include_jacobian:
-    #         error_jacobian = -stm_of[0:4, 4:8] # 4x4 : -d(pos_vec_f_mns, vel_vec_f_mns) / d(copos_vec_o_pls, covel_vec_o_pls)
-    # elif case_choice == 2:
-    #     error = np.hstack([error_pos_vec_f, error_vel_vec_f, error_ham_f]) # 5 constraints
-    #     if include_jacobian:
-    #         ... # 5x5: -d(pos_vec_f_mns, vel_vec_f_mns, ham_f_mns) / d(time_f_mns, copos_vec_o_pls, covel_vec_o_pls)
 
-    # error = state_costate_f[:4] - np.hstack([pos_vec_f_pls, vel_vec_f_pls])
-    # if include_jacobian:
-    #     error_jacobian = stm_of[0:4, 4:8]
 
     # Return
     if include_jacobian:

one_body_trajectory_optimization/src/plot/final_results.py
@@ -82,8 +82,8 @@ def plot_final_results(
                 thrust_acc_mag_t = covel_mag_t
                 thrust_acc_mag_t = np.minimum( thrust_acc_mag_t, thrust_max / mass_t ) # max thrust-acc constraint
                 thrust_acc_mag_t = np.maximum( thrust_acc_mag_t, thrust_min / mass_t ) # min thrust-acc constraint
-            thrust_acc_x_dir_t = covel_x_t / covel_mag_t
-            thrust_acc_y_dir_t = covel_y_t / covel_mag_t
+            thrust_acc_x_dir_t = -covel_x_t / covel_mag_t
+            thrust_acc_y_dir_t = -covel_y_t / covel_mag_t
             thrust_acc_dir_t   = np.vstack([thrust_acc_x_dir_t, thrust_acc_y_dir_t])
             thrust_acc_x_t     = thrust_acc_mag_t * thrust_acc_x_dir_t
             thrust_acc_y_t     = thrust_acc_mag_t * thrust_acc_y_dir_t
@@ -99,15 +99,15 @@ def plot_final_results(
                 thrust_acc_mag_t = covel_mag_t
                 thrust_acc_mag_t = np.minimum( thrust_acc_mag_t, thrust_acc_max ) # max thrust-acc constraint
                 thrust_acc_mag_t = np.maximum( thrust_acc_mag_t, thrust_acc_min ) # min thrust-acc constraint
-            thrust_acc_x_dir_t = covel_x_t / covel_mag_t
-            thrust_acc_y_dir_t = covel_y_t / covel_mag_t
+            thrust_acc_x_dir_t = -covel_x_t / covel_mag_t
+            thrust_acc_y_dir_t = -covel_y_t / covel_mag_t
             thrust_acc_dir_t   = np.vstack([thrust_acc_x_dir_t, thrust_acc_y_dir_t])
             thrust_acc_x_t     = thrust_acc_mag_t * thrust_acc_x_dir_t
             thrust_acc_y_t     = thrust_acc_mag_t * thrust_acc_y_dir_t
             thrust_acc_vec_t   = np.vstack([thrust_acc_x_t, thrust_acc_y_t])
         else: # assume no thrust nor thrust-acc constraints
             # Thrust or thrust-acc constraints
-            thrust_acc_vec_t = np.array([ covel_x_t, covel_y_t ])
+            thrust_acc_vec_t = np.array([ -covel_x_t, -covel_y_t ])
             thrust_acc_dir_t = thrust_acc_vec_t / np.linalg.norm(thrust_acc_vec_t, axis=0, keepdims=True)
             thrust_acc_mag_t = np.sqrt( thrust_acc_vec_t[0]**2 + thrust_acc_vec_t[1]**2 )
     thrust_mag_t = mass_t * thrust_acc_mag_t