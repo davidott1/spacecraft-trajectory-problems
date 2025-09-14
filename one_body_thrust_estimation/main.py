55094e92a80d9fb127c06f54d8cda0305cf9e83e
davidottesen <davidottesen@gmail.com>
2025-09-14 03:48:18 -0500

    adding new function

---
+++ one_body_thrust_estimation/main.py
@@ -0,0 +1,17 @@
+# Imports
+import numpy as np
+
+def main():
+    
+    # Start thrust estimation program
+    print(2*"\n"+"==========================")
+    print(       "THRUST ESTIMATION PROGRAM")
+    print(       "==========================")
+
+    # End thrust estimation program
+    print()
+    return True
+
+if __name__ == '__main__':
+    main()
+
